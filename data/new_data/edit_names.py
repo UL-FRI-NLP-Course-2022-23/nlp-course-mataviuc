a6c1d0a29473c07d07881045fdabb7b27edd2f92
Nina Velikajne <nv6920@student.uni-lj.si>
2023-05-06 11:21:59 +0200

    Added 30 fables+ann

---
+++ data/new_data/edit_names.py
@@ -0,0 +1,15 @@
+import os
+
+dir_path = './original/'
+for story_name in os.listdir(dir_path):
+    old_path=dir_path+story_name
+    new_name=story_name.replace(" ","_")
+    new_path=dir_path+new_name
+    os.rename(old_path,new_path)
+
+dir_path = './annotations/'
+for story_name in os.listdir(dir_path):
+    old_path=dir_path+story_name
+    new_name=story_name.replace(" ","_")
+    new_path=dir_path+new_name
+    os.rename(old_path,new_path)
