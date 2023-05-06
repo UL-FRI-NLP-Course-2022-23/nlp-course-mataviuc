8dd15776fe6358ba91c373577ea2f56cda7516d8
Nina Velikajne <nv6920@student.uni-lj.si>
2023-05-06 11:47:17 +0200

    Edited corpus analysis

--- data/new_data/edit_names.py
+++ data/new_data/edit_names.py
@@ -3,13 +3,13 @@ import os
 dir_path = './original/'
 for story_name in os.listdir(dir_path):
     old_path=dir_path+story_name
-    new_name=story_name.replace(" ","_")
+    new_name=story_name.replace(" ","_").replace(",","")
     new_path=dir_path+new_name
     os.rename(old_path,new_path)
 
 dir_path = './annotations/'
 for story_name in os.listdir(dir_path):
     old_path=dir_path+story_name
-    new_name=story_name.replace(" ","_")
+    new_name=story_name.replace(" ","_").replace(",","")
     new_path=dir_path+new_name
     os.rename(old_path,new_path)
