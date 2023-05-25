c9fd2bc5ede4861f187dfe7e18bb94bbc7a0f888
Nina Velikajne <nv6920@student.uni-lj.si>
2023-05-25 09:47:18 +0200

    added single sentiment to graph

--- src/coocurance.py
+++ src/coocurance.py
@@ -6,10 +6,16 @@ import numpy as np
 import pandas as pd
 from nltk.tokenize import sent_tokenize
 
-colors_dict={
+colors_dict_edges={
     1: '#A1D573',
     -1: '#E46425',
     0: '#F5B521'
+}
+
+colors_dict_nodes={
+    1: '#78A054',
+    -1: '#B34E1D',
+    0: '#BC8C1A'
 }
 
 def draw_graph(story, characters, story_name, sentiment,plot=True):
@@ -43,16 +49,22 @@ def draw_graph(story, characters, story_name, sentiment,plot=True):
         for u,v,d in G.edges(data=True):
             c1=characters[u]
             c2=characters[v]
-            d['color'] = colors_dict[sentiment[c1][c2]]
+            d['color'] = colors_dict_edges[sentiment[c1][c2]]
 
         # for character1 in G.nodes():
         #     for character2 in G.nodes():
         #         G[character1][character2]['color']=sentiment[character1][character2]
 
+        colors_node = []
+        for n in G:
+            c=characters[n]
+            colors_node.append(colors_dict_nodes[sentiment[c]['overall']])
+
         edges = G.edges()
         weights = [G[u][v]['weight'] for u, v in edges]
-        colors = [G[u][v]['color'] for u, v in edges]
-        nx.draw(G, ax=ax,labels=l, with_labels=True, width=weights, font_size=18, edge_color=colors, node_color='#5C5B58')
+        colors_edge = [G[u][v]['color'] for u, v in edges]
+
+        nx.draw(G, ax=ax,labels=l, with_labels=True, node_color=colors_node, width=weights, font_size=18, edge_color=colors_edge)
 
         ax.set_title(story_name.replace('.txt', '').replace('_',' '))
         plt.savefig("../results/figs/" + story_name.replace('txt', 'png'))
@@ -62,7 +74,7 @@ def draw_graph(story, characters, story_name, sentiment,plot=True):
 
 
 dir_path = '../data/aesop/coreferenced/'
-for story_name in os.listdir(dir_path):
+for story_name in ['The_Cat_Maiden.txt','The_Hare_With_Many_Friends.txt']:#os.listdir(dir_path):
     with open(dir_path + story_name, 'r') as file:
         story = file.read().replace('\n', ' ')
 
